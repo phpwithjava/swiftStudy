c7d47fe39aeae7b145e8c15247665c5f1b2aa124
phpwithjava <mysql_connect@naver.com>
2019-10-05 16:12:50 +0900

    First Study

--- study9.playground/Contents.swift
+++ study9.playground/Contents.swift
@@ -16,6 +16,25 @@ for i in 0..<100{
     print(i)
 }
 
-for i in (0..<100).reversed(){
+for i in (0...100).reversed(){
+    i
+    print(i+2)
+}
+
+for i in stride(from: 0, to: 100, by: 3).reversed(){
     print(i)
+}
+
+//배열 Array
+var arrayInt = [1,2,3,4,5]
+
+for i in arrayInt{
+    print(i)
+}
+
+//딕셔너리 Dictionary
+let person = ["john":100,"tom":200]
+for (name,money) in person{
+    print(name)
+    print(money)
 }
