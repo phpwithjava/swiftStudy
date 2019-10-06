819182480d65179dc95f7a653a8e32b028eb4b3f
phpwithjava <mysql_connect@naver.com>
2019-10-06 13:24:29 +0900

    Second Study

--- study10.playground/Contents.swift
+++ study10.playground/Contents.swift
@@ -9,7 +9,32 @@ import UIKit
 
 //swift에서는
 //값 타입 : 구조체, 기본 자료형(Int, Float, String, Bool)이 구조체,
-//      열거형
-//참조 타입: 클래스
+//      열거형(Enum),딕셔너리(Dictionary),배열(Array), 집합(Set)
+//참조 타입: 클래스,NS류의 자료형들(레거시 Objective-C에서 사용) : NSString, NSArray, NSNumber : NS(Next Step)
 
-//
+//스위프트는 값 타입을 선호함.
+
+//구조체(Struct):변수와 함수, 값타입, 익스텐션, 값 전달이 간편
+//클래스(Class):변수와 함수, 참조타입, 상속가능(override), 익스텐션(기능확장), 값 복사가 불편
+//열거형(Enum):변수와 함수, 값타입, 익스텐션
+
+struct MyStruct{
+    var age  = 30
+    
+}
+
+class MyClass{
+    var age = 30
+}
+
+//call by value
+let myStruct = MyStruct() //new 없어짐
+var myStruct2 = myStruct // 복사 : 값도 함께 복사됨
+myStruct2.age = 33
+print(myStruct.age)
+
+//call by reference
+let myClass = MyClass()
+var myClass2 = myClass
+myClass2.age = 33
+print(myClass.age)
---
+++ study11.playground/Contents.swift
@@ -0,0 +1,112 @@
+import UIKit
+
+//문자와 문자열 9장 139쪽
+
+//String타입 : 스위프트 구조체 타입
+//NSString타입 : NS계열 클래스 타입
+
+var myString:String = "hong"
+//다중 문자열
+var multipleString :String = """
+다중
+문자열
+입니다
+"""
+
+//문자열의 길이
+var myName:String = "hong"
+print(myName.count)
+
+//문자열의 비교
+let str1:String = "Apple"
+let str2:String = "Banana"
+
+if str1 == str2{ // .equals() 함수 안써도 됨
+    print("문자열이 서로 같습니다")
+}else{
+    print("문자열이 서로 다릅니다")
+}
+let result = str1.compare(str2)
+switch result{
+case .orderedSame:
+    print("같음")
+case .orderedAscending, .orderedDescending:
+    print("값이 다름")
+default:
+    print("그외")
+}
+//문자열의 연결
+print(str1 + str2)
+print(str1.appending(str2))
+
+//문자열에서 특정 문자 하나를 가져올 때
+let text = "abc" //타입 추정에 의해서 String
+let index1:String.Index =  text.index(text.startIndex, offsetBy: 1)
+let char:Character = text[index1]
+
+
+for ch in text{
+    print(ch)
+}
+var index2 = 0
+for ch in text{
+    if(index2==1){
+        print(ch)
+    }
+    index2 += 1
+    
+}
+
+let char2:Character = "A"
+//let char3:Character = "AB" //Error
+let char4 = Character("A")
+
+//문자열애 추가
+var myName2:String = "tom"
+//문자를 하나 추가함
+myName2.insert("!", at: myName2.endIndex)
+//문자열을 추가함.
+myName2.insert(contentsOf: "My", at: myName2.index(myName2.startIndex, offsetBy: 0))
+
+let myName3 = "My" + myName2
+
+//형변환 NSString -> String
+let myNSString:NSString = "john"
+let myString2:String = String(myNSString)
+let myString3:String = myNSString as String
+
+//문자열의 치환(바꾸기)
+var myString4:String = "Swift Programming"
+let resultString1:String = myString4.replacingOccurrences(of: "Swift", with: "iOS")
+
+//NSString(변경불가) NSMutableString(변경가능)
+//String,String
+
+//문자열 삭제
+var myString5:String = "Swift Programming"
+myString5.removeAll()
+myString5 = ""
+
+//문자 하나를 지움
+var myString6:String = "Swift Programming"
+let resultString2 = myString6.remove(at: myString6.startIndex)
+print(myString6)
+
+//문자열을 지움
+var myString7:String = "Swift Programming"
+let removeRange = myString7.index(myString7.startIndex, offsetBy: 0) ... myString7.index(myString7.startIndex, offsetBy: 5)
+myString7.removeSubrange(removeRange)
+print(myString7)
+
+var myString8:String = "Swift Programming"
+var idx = 0
+var appendString = ""
+for ch in myString8{
+    idx+=1
+    if((idx - 1)<=5){
+        continue
+    }
+    appendString += String(ch)
+    
+}
+print(appendString)
